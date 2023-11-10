740aaa87844bcfb02a64ddfb7ee6c70ce7620698
Iqbal Farhan Syuhada <iqbalfarhan1996@gmail.com>
2023-11-10 10:53:39 +0800

    update permissions

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $datas = [
+            'material.index',
+            'material.create',
+            'material.edit',
+            'material.delete',
+            'material.comot',
+            'material.cari',
+
+            'user.index',
+            'user.create',
+            'user.edit',
+            'user.delete',
+
+            'group.index',
+            'group.create',
+            'group.edit',
+            'group.delete',
+        ];
+
+        foreach ($datas as $data) {
+            Permission::updateOrCreate([
+                'name' => $data
+            ]);
+        }
+    }
+}
