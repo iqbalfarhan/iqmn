bc76ea6b23d4e2377e8b0600a2012d77bc0e3c32
iqbalfarhan <iqbalfarhan1996@gmail.com>
2023-11-13 15:08:33 +0800

    update favicon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>{{ $title ?? config('app.name') }}</title>
+    <link rel="icon" href="{{ url('favicon.jpg') }}">
     @vite('resources/css/app.css')
 
     @livewireStyles
