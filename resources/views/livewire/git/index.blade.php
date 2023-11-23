1c4b7bd88c2091cd611f3ac1bfee6828880fce59
iqbalfarhan <iqbalfarhan1996@gmail.com>
2023-11-24 00:33:35 +0800

    update git

--- resources/views/livewire/git/index.blade.php
+++ resources/views/livewire/git/index.blade.php
@@ -1,17 +1,22 @@
 <div class="space-y-6">
-    <div>
+    <div class="flex gap-2">
         <button class="btn btn-primary" wire:click="gitExec('stash')">
             <x-tabler-database class="w-5 h-5" />
             <span>Git stash</span>
         </button>
         <button class="btn btn-primary" wire:click="gitExec('pull')">
-            <x-tabler-database class="w-5 h-5" />
+            <x-tabler-git-merge class="w-5 h-5" />
             <span>Git pull</span>
         </button>
         <button class="btn btn-primary" wire:click="gitExec('status')">
             <x-tabler-database class="w-5 h-5" />
             <span>Git status</span>
         </button>
+        <span wire:loading>
+            <div class="btn btn-ghost">
+                <div class="loading loading-xs"></div>
+            </div>
+        </span>
     </div>
 
     <div>
