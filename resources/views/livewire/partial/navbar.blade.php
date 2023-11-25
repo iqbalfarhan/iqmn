b123b308bff16e98dc6697fbf1acbe902af6db48
IQBAL FARHAN SYUHADA <53584619+iqbalfarhan@users.noreply.github.com>
2023-11-26 02:34:36 +0800

    Update navbar.blade.php

--- resources/views/livewire/partial/navbar.blade.php
+++ resources/views/livewire/partial/navbar.blade.php
@@ -1,6 +1,6 @@
 <div class="navbar">
     <div class="navbar-start">
-        <label for="drawer" class="btn btn-ghost btn-circle flex lg:hidden">
+        <label for="drawer" class="btn btn-ghost btn-circle">
             <x-tabler-menu class="w-5 h-5" />
         </label>
     </div>
