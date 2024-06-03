7c8a3315d5704c9acfcc0de9b4b8b4fcf4331c42
iqbal syuhada <iqbalfarhan1996@gmail.com>
2024-06-03 13:38:21 +0800

    udpate group show overflow user email

--- resources/views/livewire/user/item.blade.php
+++ resources/views/livewire/user/item.blade.php
@@ -1,12 +1,12 @@
-<div class="card card-compact bg-base-200">
+<div class="card card-compact bg-base-200 overflow-hidden">
     <div class="card-body">
-        <div class="flex flex-1 gap-3">
-            <div class="avatar">
+        <div class="flex gap-3">
+            <div class="avatar flex-none">
                 <div class="size-12 rounded-full">
                     <img src="{{ $user->image_url }}" alt="">
                 </div>
             </div>
-            <div class="flex flex-col">
+            <div class="flex flex-col flex-1">
                 <div class="line-clamp-1 font-bold text-lg">{{ $user->name }}</div>
                 <div class="line-clamp-1 text-sm">{{ $user->email }}</div>
             </div>
