e3521d552e1e323d0f167ddc9cec16556703d86b
iqbal syuhada <iqbalfarhan1996@gmail.com>
2024-06-03 14:16:32 +0800

    update offline page

--- resources/views/vendor/laravelpwa/offline.blade.php
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -1,7 +1,35 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="{{ session('theme', 'dark') }}">
 
-@section('content')
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <h1>You are currently not connected to any networks.</h1>
+        <title>{{ config('app.name') }} ::. {{ $title ?? 'Iqbal material ngajar' }}</title>
 
-@endsection
+        @if (session('theme', 'dark') == 'dark')
+            <link rel="icon" href="{{ url('darkicon.png') }}">
+        @else
+            <link rel="icon" href="{{ url('lighticon.png') }}">
+        @endif
+
+        @vite('resources/css/app.css')
+
+        @livewireStyles
+        @laravelPWA
+    </head>
+
+    <body>
+        <div class="hero min-h-screen bg-base-100">
+            <div class="hero-content text-center">
+                <div class="max-w-md space-y-8">
+                    <h1 class="text-5xl font-extrabold">Kamu lagi Offline</h1>
+                    <p>Coba cek lagi koneksi internet kamu, mungkin ada beberapa kendala sehingga kamu
+                        tidak bisa mengakses</p>
+                    <h1 class="text-3xl font-black text-primary">.IQMN</h1>
+                </div>
+            </div>
+        </div>
+    </body>
+
+</html>
