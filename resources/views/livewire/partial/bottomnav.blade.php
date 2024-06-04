c00549066ab91153b7e062a80ef02896fc0b5a4e
iqbal syuhada <iqbalfarhan1996@gmail.com>
2024-06-04 14:03:21 +0800

    add active router

--- resources/views/livewire/partial/bottomnav.blade.php
+++ resources/views/livewire/partial/bottomnav.blade.php
@@ -1,23 +1,27 @@
 <div class="btm-nav border-t-2 border-base-300 md:hidden">
     <a href="{{ route('home') }}" @class(['active text-primary' => Route::is('home')]) wire:navigate>
-        <x-tabler-home class="size-7" />
+        <x-tabler-home class="size-5" />
     </a>
     <a href="{{ route('material.cari') }}" @class([
         'active text-primary' => Route::is('material.cari'),
     ]) wire:navigate>
-        <x-tabler-search class="size-7" />
+        <x-tabler-search class="size-5" />
     </a>
     <a href="{{ route('material.comot') }}" @class([
-        'active text-primary' => Route::is(['material.comot', 'material.show']),
+        'active text-primary' => Route::is([
+            'material.comot',
+            'material.show',
+            'material.quiz',
+        ]),
     ]) wire:navigate>
-        <x-tabler-book class="size-7" />
+        <x-tabler-book class="size-5" />
     </a>
     <a href="{{ route('group.mine') }}" @class([
-        'active text-primary' => Route::is('group.mine'),
+        'active text-primary' => Route::is(['group.mine', 'group.show']),
     ]) wire:navigate>
-        <x-tabler-users class="size-7" />
+        <x-tabler-users class="size-5" />
     </a>
     <a href="{{ route('profile') }}" @class(['active text-primary' => Route::is('profile')]) wire:navigate>
-        <x-tabler-user class="size-7" />
+        <x-tabler-user class="size-5" />
     </a>
 </div>
