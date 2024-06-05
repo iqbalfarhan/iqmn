146928ff7ff8679e1493def0693b9886482d334d
iqbal syuhada <iqbalfarhan1996@gmail.com>
2024-06-05 10:31:30 +0800

    update scroll

--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -1,38 +1,42 @@
-<div class="page-wrapper space-y-10">
-    <div class="grid lg:grid-cols-2 gap-2 md:gap-6">
-        @livewire('user.item', ['user' => $user])
-        @livewire('widget.tanggal')
+<div class="">
+    <div class="page-wrapper">
+        <div class="grid lg:grid-cols-2 gap-2 md:gap-6">
+            @livewire('user.item', ['user' => $user])
+            @livewire('widget.tanggal')
+        </div>
     </div>
-    <div class="grid md:hidden grid-cols-4 md:grid-cols-8 gap-4">
-        <div class="flex flex-col items-center justify-center gap-2">
-            <a href="{{ route('material.cari') }}" class="btn btn-lg btn-circle">
-                <x-tabler-search class="size-7" />
-            </a>
-            <span class="text-xs">Cari</span>
-        </div>
-        <div class="flex flex-col items-center justify-center gap-2">
-            <a href="{{ route('material.comot') }}" class="btn btn-lg btn-circle">
-                <x-tabler-book class="size-7" />
-            </a>
-            <span class="text-xs">Materiku</span>
-        </div>
-        <div class="flex flex-col items-center justify-center gap-2">
-            <a href="{{ route('group.mine') }}" class="btn btn-lg btn-circle">
-                <x-tabler-users class="size-7" />
-            </a>
-            <span class="text-xs">Groupku</span>
-        </div>
-        <div class="flex flex-col items-center justify-center gap-2">
-            <a href="{{ route('profile') }}" class="btn btn-lg btn-circle">
-                <x-tabler-user-hexagon class="size-7" />
-            </a>
-            <span class="text-xs">Profile</span>
+    <div class="page-wrapper">
+        <div class="grid md:hidden grid-cols-4 md:grid-cols-8 gap-4">
+            <div class="flex flex-col items-center justify-center gap-2">
+                <a href="{{ route('material.cari') }}" class="btn btn-lg btn-circle">
+                    <x-tabler-search class="size-7" />
+                </a>
+                <span class="text-xs">Cari</span>
+            </div>
+            <div class="flex flex-col items-center justify-center gap-2">
+                <a href="{{ route('material.comot') }}" class="btn btn-lg btn-circle">
+                    <x-tabler-book class="size-7" />
+                </a>
+                <span class="text-xs">Materiku</span>
+            </div>
+            <div class="flex flex-col items-center justify-center gap-2">
+                <a href="{{ route('group.mine') }}" class="btn btn-lg btn-circle">
+                    <x-tabler-users class="size-7" />
+                </a>
+                <span class="text-xs">Groupku</span>
+            </div>
+            <div class="flex flex-col items-center justify-center gap-2">
+                <a href="{{ route('profile') }}" class="btn btn-lg btn-circle">
+                    <x-tabler-user-hexagon class="size-7" />
+                </a>
+                <span class="text-xs">Profile</span>
+            </div>
         </div>
     </div>
     @if ($user->groups->count() > 0)
-        <div class="space-y-4">
-            <h1 class="text-xl">Groupku <small>({{ $user->groups->count() }} group)</small></h1>
-            <div class="flex flex-col gap-2">
+        <div class="space-y-4 py-6">
+            <h1 class="text-xl px-6">Groupku <small>({{ $user->groups->count() }} group)</small></h1>
+            <div class="flex flex-col gap-2 px-6">
                 @foreach ($user->groups as $group)
                     @livewire('group.item', ['group' => $group, 'showToggleJoin' => false], key($group->id))
                 @endforeach
@@ -40,9 +44,9 @@
         </div>
     @endif
     @if ($user->materials->count() > 0)
-        <div class="space-y-4">
-            <h1 class="text-xl">Material belajarku <small>({{ $user->materials->count() }} materi)</small></h1>
-            <div class="flex overflow-x-auto scrollbar-hide gap-4">
+        <div class="space-y-4 py-6">
+            <h1 class="text-xl px-6">Material belajarku <small>({{ $user->materials->count() }} materi)</small></h1>
+            <div class="flex overflow-x-auto scrollbar-hide gap-4 px-6">
                 @foreach ($user->materials as $material)
                     <div class="min-w-80 max-w-80">
                         @livewire('material.item', ['material' => $material], key($material->id))
