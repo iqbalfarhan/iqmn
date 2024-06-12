5c3b6cae11e41b9bee4c327087b501d76802215f
iqbalfarhan <iqbalfarhan1996@gmail.com>
2024-06-13 01:27:40 +0800

    update read

--- resources/views/livewire/pages/article.blade.php
+++ resources/views/livewire/pages/article.blade.php
@@ -7,8 +7,8 @@
         <div class="flex items-center opacity-75">
             <div class="flex gap-2 flex-1">
                 <div class="avatar size-5">
-                    <div class="w-5 rounded-full">
-                        <img src="{{ $post->user->image_url }}" alt="">
+                    <div class="w-5 rounded-full bg-base-300">
+                        <img src="{{ $post->user->image_url }}" alt="A">
                     </div>
                 </div>
                 <div class="text-sm">{{ $post->user->name }}</div>
