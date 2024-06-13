44b73950f534af40af85522cfe375473a57cabb6
iqbalfarhan <iqbalfarhan1996@gmail.com>
2024-06-13 22:30:57 +0800

    update user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -24,7 +24,7 @@ class UserSeeder extends Seeder
         $user->assignRole('superadmin');
 
         User::factory(10)->create()->each(function ($baru) {
-            $baru->assignRole('user');
+            $baru->assignRole('reader');
         });
     }
 }
