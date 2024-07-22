0a0557c67e63bc61e175551e6bdd1a3b2398eec9
IQBAL FARHAN SYUHADA <iqbalfarhan1996@gmail.com>
2024-07-22 10:10:01 +0800

    material update

--- database/migrations/2024_07_21_224712_add_media_to_quizzes_table.php
+++ database/migrations/2024_07_21_224712_add_media_to_quizzes_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('quizzes', function (Blueprint $table) {
-            $table->foreignId('material_id')->nullable()->constrained()->onDelete('cascade');
             $table->foreignId('ujian_id')->nullable()->constrained()->onDelete('cascade');
             $table->string('media')->nullable();
         });
