f8b9a1c9e275a4ca27a430377eccbd0ba43837f8
iqbalfarhan <iqbalfarhan1996@gmail.com>
2024-11-20 12:59:00 +0800

    update default theme

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="{{ session('theme', 'cupcake') }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="{{ session('theme', 'dracula') }}">
 
     <head>
         <meta charset="utf-8">
